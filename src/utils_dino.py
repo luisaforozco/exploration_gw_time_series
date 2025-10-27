2ccb59f2054d87f9eec763b42f3f8aa38d958463
Luisa Orozco <luisitafer14@gmail.com>
2025-10-27 12:15:00 +0100

    Add new functions and further study on querying DINO

--- src/utils_dino.py
+++ src/utils_dino.py
@@ -23,60 +23,72 @@ def get_migrated_data_by_bro_id(bro_GLD_id: str, engine=None) -> pd.DataFrame:
     """
     if engine is None:
         engine = init_connection_to_dino()
-    sql_query_migrated_GLD = f"""
-    WITH
-    cte_migration_event as (
-    SELECT
-        GWS_W.WELL_DBK
-    , GWS_W.NITG_NR
-    , GWS_PZM.PIEZOMETER_DBK
-    , GWS_PZM.PIEZOMETER_NR
-    , BRO_M_E.EVENT_DBK
-    , BRO_M_E.BRO_ID
-    , BRO_M_E.BRO_DETAIL_ID AS observationId
-    , LOC.X_RD_CRD
-    , LOC.Y_RD_CRD
-    FROM
-        DINO_DBA.GWS_WELL GWS_W
-        INNER JOIN DINO_DBA.GWS_PIEZOMETER GWS_PZM
-        on GWS_W.WELL_DBK = GWS_PZM.WELL_DBK
-        INNER JOIN DINO_DBA.BRO_MIGRATION_EVENT BRO_M_E
-        on GWS_PZM.PIEZOMETER_DBK = BRO_M_E.EVENT_RECORD_DBK
-        INNER JOIN DINO_DBA.LOC_SURFACE_LOCATION LOC 
-        on LOC.SURFACE_LOCATION_DBK = GWS_W.SURFACE_LOCATION_DBK
-    WHERE
-        BRO_M_E.BRO_ID = '{bro_GLD_id}'
-        and BRO_M_E.RO_TYPE_CD = 'GLD'
-        and BRO_M_E.EVENT_TYPE_CD = 'ADDITION'
-        and BRO_M_E.TABLE_NM_DBK = (SELECT TABLE_NM_DBK FROM DINO_DBA.REF_BRO_MIGRATION_TABLE_NM WHERE TABLE_NM = 'GWS_PIEZOMETER')
-    )
-    SELECT
-    cte_m_e.NITG_NR
-    , cte_m_e.PIEZOMETER_NR
-    , cte_m_e.BRO_ID
-    , cte_m_e.observationId
-    , cte_m_e.X_RD_CRD
-    , cte_m_e.Y_RD_CRD
-    , GWS_MSM_H.MONITOR_DATE
-    , GWS_MSM_H.VALUE
-    FROM
-    cte_migration_event cte_m_e
-    INNER JOIN DINO_DBA.BRO_MIGRATION_RECORD BRO_M_R
-        on cte_m_e.EVENT_DBK = BRO_M_R.EVENT_DBK
-    INNER JOIN DINO_DBA.GWS_MSM_HEAD GWS_MSM_H
-        on BRO_M_R.MIGRATED_RECORD_DBK = GWS_MSM_H.MSM_HEAD_DBK
-    WHERE
-    BRO_M_R.TABLE_NM_DBK = (SELECT TABLE_NM_DBK FROM DINO_DBA.REF_BRO_MIGRATION_TABLE_NM WHERE TABLE_NM = 'GWS_MSM_HEAD')
-    and BRO_M_R.MIGRATION_STATUS_CD in ('REWORK', 'SUCCESS')
-    ORDER BY
-    cte_m_e.WELL_DBK
-    , cte_m_e.PIEZOMETER_DBK
-    , GWS_MSM_H.MONITOR_DATE
+    sql_query = f"""
+        WITH
+        cte_migration_event as (
+        SELECT
+        GWS_W.NITG_NR
+        , GWS_PZM.PIEZOMETER_DBK
+        , BRO_M_E.EVENT_DBK
+        , BRO_M_E.BRO_ID
+        , BRO_M_E.BRO_DETAIL_ID
+        , LOC.X_RD_CRD
+        , LOC.Y_RD_CRD
+        FROM
+            DINO_DBA.GWS_WELL GWS_W
+            INNER JOIN DINO_DBA.GWS_PIEZOMETER GWS_PZM
+                ON GWS_W.WELL_DBK = GWS_PZM.WELL_DBK
+            INNER JOIN DINO_DBA.BRO_MIGRATION_EVENT BRO_M_E
+                ON GWS_PZM.PIEZOMETER_DBK = BRO_M_E.EVENT_RECORD_DBK
+            INNER JOIN DINO_DBA.LOC_SURFACE_LOCATION LOC 
+                ON LOC.SURFACE_LOCATION_DBK = GWS_W.SURFACE_LOCATION_DBK
+        WHERE
+            BRO_M_E.BRO_ID = '{bro_GLD_id}'
+            AND BRO_M_E.RO_TYPE_CD = 'GLD'
+            AND BRO_M_E.EVENT_TYPE_CD = 'ADDITION'
+            AND BRO_M_E.TABLE_NM_DBK = (SELECT TABLE_NM_DBK FROM DINO_DBA.REF_BRO_MIGRATION_TABLE_NM WHERE TABLE_NM = 'GWS_PIEZOMETER')
+        )
+        SELECT
+        cte_m_e.NITG_NR
+        , cte_m_e.PIEZOMETER_DBK
+        , cte_m_e.BRO_ID
+        , cte_m_e.BRO_DETAIL_ID
+        , cte_m_e.X_RD_CRD as X
+        , cte_m_e.Y_RD_CRD as Y
+        , GWS_MSM_H.MONITOR_DATE
+        , GWS_MSM_H.VALUE
+        ,  (
+            SELECT gph.MSM_NAP_HEIGHT
+            FROM DINO_DBA.GWS_PIE_HISTORY gph
+            WHERE gph.PIEZOMETER_DBK = cte_m_e.PIEZOMETER_DBK
+            AND gph.START_DATE = (
+                    SELECT MAX(gph2.START_DATE)
+                    FROM DINO_DBA.GWS_PIE_HISTORY gph2
+                    WHERE gph2.PIEZOMETER_DBK = cte_m_e.PIEZOMETER_DBK AND gph2.START_DATE <= GWS_MSM_H.MONITOR_DATE
+            )
+            ) AS MSM_NAP_HEIGHT
+        FROM cte_migration_event cte_m_e
+        INNER JOIN DINO_DBA.BRO_MIGRATION_RECORD BRO_M_R
+            ON cte_m_e.EVENT_DBK = BRO_M_R.EVENT_DBK
+        INNER JOIN DINO_DBA.GWS_MSM_HEAD GWS_MSM_H
+            ON BRO_M_R.MIGRATED_RECORD_DBK = GWS_MSM_H.MSM_HEAD_DBK
+        INNER JOIN DINO_DBA.GWS_PIE_HISTORY GWS_PH 
+            ON GWS_PH.PIEZOMETER_DBK = cte_m_e.PIEZOMETER_DBK
+        WHERE
+            BRO_M_R.TABLE_NM_DBK = (SELECT TABLE_NM_DBK FROM DINO_DBA.REF_BRO_MIGRATION_TABLE_NM WHERE TABLE_NM = 'GWS_MSM_HEAD')
+            AND BRO_M_R.MIGRATION_STATUS_CD in ('REWORK', 'SUCCESS')
+        ORDER BY
+            GWS_MSM_H.MONITOR_DATE
     """
-    df = pd.read_sql(sql_query_migrated_GLD, engine)
+    df = pd.read_sql(sql_query, engine)
+    if df.empty: 
+        raise ValueError(f"No data found for BRO_ID: {bro_GLD_id}")
     df['monitor_date'] = df['monitor_date'].dt.tz_localize('Europe/Amsterdam')
     df['monitor_date'] = parse_date_to_unix(df['monitor_date'])
-    #TODO: correct values to get . t.ov. NAP
+    # convert values to meters above NAP
+    df['value'] = (df['msm_nap_height'] - df['value'])/100
+    # delete msm_nap_height column
+    df = df.drop(columns=['msm_nap_height'])
     return df
 
 
@@ -119,3 +131,49 @@ def parse_date_to_unix(timestamps, default_tz='UTC'):
     else:
         raise ValueError(f"Cannot convert timestamps of type {type(ts)}")
     return unix_times
+
+
+def get_DINO_data_by_piezometer(piezometer_dbk, engine=None) -> pd.DataFrame:
+    """
+    Fetch data from DINO by piezometer_dbk.
+    Args:
+        piezometer_dbk: Piezometer DBK identifier as a string.
+        engine: SQLAlchemy engine instance. If None, a new connection will be initialized.
+    Returns:
+        pd.DataFrame: DataFrame containing the data with 'monitor_date' converted to Unix time and values adjusted to meters above NAP.
+    """
+    sql = f"""
+        SELECT
+            w.NITG_NR,
+            l.X_RD_CRD as X,
+            l.Y_RD_CRD as Y,
+            p.PIEZOMETER_NR,
+            h.MONITOR_DATE,
+            h.VALUE,
+            gph.MSM_NAP_HEIGHT
+        FROM
+            DINO_DBA.GWS_MSM_HEAD h
+        INNER JOIN
+            DINO_DBA.GWS_PIEZOMETER p ON h.piezometer_dbk = p.piezometer_dbk
+        INNER JOIN
+            DINO_DBA.GWS_PIE_HISTORY gph ON p.piezometer_dbk = gph.PIEZOMETER_DBK
+        INNER JOIN
+            DINO_DBA.GWS_WELL w ON p.well_dbk = w.well_dbk
+        INNER JOIN
+            DINO_DBA.LOC_SURFACE_LOCATION l ON l.SURFACE_LOCATION_DBK = w.SURFACE_LOCATION_DBK
+        WHERE
+            h.piezometer_dbk  = '{piezometer_dbk}'
+    """
+    if engine is None:
+        engine = init_connection_to_dino()
+
+    df = pd.read_sql(sql, engine)
+    if df.empty: 
+        raise ValueError(f"No data found for BRO_ID: {bro_id}")
+    df['monitor_date'] = df['monitor_date'].dt.tz_localize('Europe/Amsterdam')
+    df['monitor_date'] = parse_date_to_unix(df['monitor_date'])
+    # convert values to meters above NAP
+    df['value'] = (df['msm_nap_height'] - df['value'])/100
+    # delete msm_nap_height column
+    df = df.drop(columns=['msm_nap_height'])
+    return df
