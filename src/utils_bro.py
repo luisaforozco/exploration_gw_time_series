026c3b61eaa2459b7ed314a4fbabc695cecdbc30
Luisa Orozco <luisitafer14@gmail.com>
2025-11-18 16:55:12 +0100

    Add fucntionality to retrieve coordinates of a well in BRO

--- src/utils_bro.py
+++ src/utils_bro.py
@@ -1,5 +1,7 @@
+import re
 import requests
 import pandas as pd
+import xml.etree.ElementTree as ET
 from io import StringIO
 
 def get_bro_data(bro_GLD_id: str) -> pd.DataFrame:
@@ -9,7 +11,7 @@ def get_bro_data(bro_GLD_id: str) -> pd.DataFrame:
     if response.status_code == 400:
         error_data = response.json()
         description = error_data.get('description', 'Unknown error')
-        raise ValueError(f"Bad request for BRO ID {bro_GLD_id}: {description}")
+        raise ValueError(f"Bad request seriesAsCsv for BRO ID {bro_GLD_id}: {description}")
     # Check if response body is empty or whitespace, TODO: decide if return an empty df or raise an error
     if not response.text.strip(): 
         raise ValueError(f"Er is geen data beschikbaar voor dit dossier {bro_GLD_id}. The response body is empty.")
@@ -25,3 +27,35 @@ def clean_csv(df: pd.DataFrame) -> pd.DataFrame:
     # remove rows containing NaNs
     df_clean.dropna(inplace=True)
     return df_clean
+
+def get_gmw_of_gld(bro_GLD_id: str) -> str:
+    headers = {"accept": "text/plain"}
+    response = requests.get(f"https://publiek.broservices.nl/gm/gld/v1/objectsAsCsv/{bro_GLD_id}?rapportagetype=volledig", headers=headers)
+    if response.status_code == 400:
+        error_data = response.json()
+        description = error_data.get('description', 'Unknown error')
+        raise ValueError(f"Bad request objectsAsCsv for BRO ID {bro_GLD_id}: {description}")
+    gmw_matches = re.search(r'\b(GMW[0-9]+)\b', response.text)
+    if gmw_matches: return gmw_matches.group(1)
+    else: raise ValueError(f"No GMW_ID found for {bro_GLD_id}")
+    
+def get_coordinates_gmw(bro_GMW_id:str) -> tuple:
+    response = requests.get(f"https://int-publiek.broservices.nl/gm/gmw/v1/objects/{bro_GMW_id}?fullHistory=", headers= {"accept": "application/xml"})
+    if response.status_code == 400:
+        error_data = response.json()
+        description = error_data.get('description', 'Unknown error')
+        raise ValueError(f"Bad request objects for BRO GMW ID {bro_GMW_id}: {description}")
+    xml = response.text
+    root = ET.fromstring(xml)
+    ns = {
+        "gml": "http://www.opengis.net/gml/3.2",
+        "gmwcommon": "http://www.broservices.nl/xsd/gmwcommon/1.1",
+        "brocom": "http://www.broservices.nl/xsd/brocommon/3.0",
+        "dsgmw": "http://www.broservices.nl/xsd/dsgmw/1.1",
+    }
+    pos = root.find(".//gmwcommon:location/gml:pos", ns)
+    x, y = map(float, pos.text.split())
+    if x is not None and y is not None:
+        return x, y
+    else:
+        raise ValueError(f"Coordinates not found for GMW ID {bro_GMW_id}")
