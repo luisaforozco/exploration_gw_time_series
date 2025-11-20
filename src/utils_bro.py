c43d86fb095ae149a0d23f232aec6d45714eb8b5
Luisa Orozco <luisitafer14@gmail.com>
2025-11-20 14:16:04 +0100

    Ammend coordinates query from BRO

--- src/utils_bro.py
+++ src/utils_bro.py
@@ -4,6 +4,7 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from io import StringIO
 
+
 def get_bro_data(bro_GLD_id: str) -> pd.DataFrame:
     headers = {"accept": "text/plain"}
     response = requests.get(f"https://publiek.broservices.nl/gm/gld/v1/seriesAsCsv/{bro_GLD_id}", headers=headers)
@@ -28,6 +29,7 @@ def clean_csv(df: pd.DataFrame) -> pd.DataFrame:
     df_clean.dropna(inplace=True)
     return df_clean
 
+
 def get_gmw_of_gld(bro_GLD_id: str) -> str:
     headers = {"accept": "text/plain"}
     response = requests.get(f"https://publiek.broservices.nl/gm/gld/v1/objectsAsCsv/{bro_GLD_id}?rapportagetype=volledig", headers=headers)
@@ -39,14 +41,18 @@ def get_gmw_of_gld(bro_GLD_id: str) -> str:
     if gmw_matches: return gmw_matches.group(1)
     else: raise ValueError(f"No GMW_ID found for {bro_GLD_id}")
     
+
 def get_coordinates_gmw(bro_GMW_id:str) -> tuple:
-    response = requests.get(f"https://int-publiek.broservices.nl/gm/gmw/v1/objects/{bro_GMW_id}?fullHistory=", headers= {"accept": "application/xml"})
-    if response.status_code == 400:
-        error_data = response.json()
-        description = error_data.get('description', 'Unknown error')
-        raise ValueError(f"Bad request objects for BRO GMW ID {bro_GMW_id}: {description}")
+    response = requests.get(f"https://publiek.broservices.nl/gm/gmw/v1/objects/{bro_GMW_id}?fullHistory=", headers= {"accept": "application/xml"})
     xml = response.text
     root = ET.fromstring(xml)
+    if response.status_code == 400:
+        try:
+            desc = root.find(".//brocom:rejectionReason", ns)
+            description = desc.text if desc is not None else xml
+        except Exception:
+            description = xml
+        raise ValueError(f"Bad request for {bro_GMW_id}: {description}")
     ns = {
         "gml": "http://www.opengis.net/gml/3.2",
         "gmwcommon": "http://www.broservices.nl/xsd/gmwcommon/1.1",
